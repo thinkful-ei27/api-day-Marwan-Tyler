650cfb3604eaafed3da5f023304f1bc75686e356
Tyler Crabb <tydacrabb@gmail.com>
2018-12-12 08:57:55 -0800

    finished number 2

--- scripts/api.js
+++ scripts/api.js
@@ -4,8 +4,9 @@ const api = (function() {
 
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/Marwan-Tyler';
   const getItems = function(callback){
-    callback('api module works!');
+    $.getJSON(`${BASE_URL}/items`, callback);
   };
+  
 
   return{
     BASE_URL,
