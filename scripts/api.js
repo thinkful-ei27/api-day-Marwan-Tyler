6f887bda11275827de6a90a166c9fa757fb5d39e
Marwan Alaloosi <manoalosi@gmail.com>
2018-12-12 11:09:49 -0600

    created .Ajax and tested successfully

--- scripts/api.js
+++ scripts/api.js
@@ -6,11 +6,27 @@ const api = (function() {
   const getItems = function(callback){
     $.getJSON(`${BASE_URL}/items`, callback);
   };
+
+  const createItem = function(name, callback){
+    const newItem = JSON.stringify({
+     name: name
+
+    });
+
+  $.ajax({
+    url: `${BASE_URL}/items`,
+    method: 'POST',
+    contentType: 'application/json',
+    data: newItem,
+    success: callback
+  })
+  }
   
 
   return{
     BASE_URL,
-    getItems
+    getItems,
+    createItem
   };
 
 }());
